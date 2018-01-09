00772a1c15b2d2b1fd092d09480f0fdef675dd32
JessicaMP <jessica95.mp@gmail.com>
2018-01-09 12:59:55 -0500

    Agregando vinculacion con google

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-$( document ).ready(function(){
-  $(".button-collapse").sideNav();
-  $('.modal').modal();
-});
-
 /*function begin() {
   setTimeout(function() {
     $('#splash').fadeOut();
@@ -11,3 +6,42 @@ $( document ).ready(function(){
 };
 
 $(document).ready(begin);*/
+
+$( document ).ready(function(){
+  $(".button-collapse").sideNav();
+  $('.modal').modal();
+});
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDtSqdUIHhBQc0hW_qr7RB0Eou_GxHrLAI",
+    authDomain: "social-green.firebaseapp.com",
+    databaseURL: "https://social-green.firebaseio.com",
+    projectId: "social-green",
+    storageBucket: "social-green.appspot.com",
+    messagingSenderId: "450259756672"
+  };
+  firebase.initializeApp(config);
+
+  var user = null;
+
+  var $loginBtn = $('#start-login');
+
+  $loginBtn.on('click', googleLogin);
+
+  function googleLogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    //esta es la doc de firebase
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        //guardamos el usuario que nos trae resuslt
+        user = result.user;
+        //mostramos su contenido
+        console.log(user);
+        //ocultamos el div de login
+        $('#login').fadeOut();
+      });
+  }
