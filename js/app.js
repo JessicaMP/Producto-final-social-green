62d99952541f4d9483abd0abe7f9432956a5ab6c
JessicaMP <jessica95.mp@gmail.com>
2018-01-08 19:40:40 -0500

    Modal log in

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-function begin() {
+$( document ).ready(function(){
+  $(".button-collapse").sideNav();
+  $('.modal').modal();
+});
+
+/*function begin() {
   setTimeout(function() {
     $('#splash').fadeOut();
     $('#page-start').removeClass('hide');
   }, 3500);
 };
 
-$(document).ready(begin);
+$(document).ready(begin);*/
