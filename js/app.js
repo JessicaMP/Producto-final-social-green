78e0063a9c0fb3cb4a0799d1b0f4aedb394010d0
JessicaMP <jessica95.mp@gmail.com>
2018-01-09 17:52:37 -0500

    validando con e mail

--- js/app.js
+++ js/app.js
@@ -47,3 +47,61 @@ $( document ).ready(function(){
         $('#login').fadeOut();
       });
   }
+
+// Vinculando con Email
+  function registrar() {
+   var email = document.getElementById('email').value;
+   var contrasena = document.getElementById('contrasena').value;
+
+     firebase.auth().createUserWithEmailAndPassword(email, contrasena).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      // ...
+    });
+  }
+
+  function ingreso() {
+    var email2 = document.getElementById('email').value;
+    var contrasena2 = document.getElementById('contrasena').value;
+      firebase.auth().signInWithEmailAndPassword(email2, contrasena2).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      // ...
+    });
+  }
+
+  function observador() {
+
+      firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log('existe usuario');
+        aparece();
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        // ...
+      } else {
+        // User is signed out.
+        console.log('no existe usuario');
+      }
+    });
+  }
+  observador();
+
+
+  function aparece() {
+    var contenido = document.getElementById('contenido');
+      contenido.innerHTML = 'solo lo ve usuario activo';
+
+  }
