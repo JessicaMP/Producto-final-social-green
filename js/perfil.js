02c03298a025835b0e1d2eb37cb343637dfbe3fa
JessicaMP <jessica95.mp@gmail.com>
2018-01-10 06:26:43 -0500

    Realizando post funcionalidad

--- js/perfil.js
+++ js/perfil.js
@@ -3,3 +3,57 @@ $(document).ready(function(){
    $(".button-collapse").sideNav();
    $('.modal').modal();
  });
+
+ $('#btn-share-post').click(function() {
+   var firstBox= $('<div></div>');
+   var content = $('<p></p>');
+   content.text($('#texto').val());
+   var time = $('<p></p>');
+   time.text(timeDate());
+
+   firstBox.addClass('styleBox');
+   content.addClass('styleLetter');
+   time.addClass('styleLetter');
+
+   if ($('#texto').val('') == true) {
+     console.log(  $('this'));
+     $('this').find('#button').disabled();
+
+   }else{
+     firstBox.append(content);
+     firstBox.append(time);
+     $('#show-post').append(firstBox);
+
+   }
+   $('#texto').val(' ');
+   texto.focus();
+ });
+
+ //Cambio de color del boton al ingresar texto
+ texto.addEventListener('keyup', function() {
+   var contenido = texto.value;
+   var length = texto.value.lenght;
+   if (contenido == '') {
+     button.className = 'Original';
+   }else {
+     button.className = 'buttonColor';
+     texto.style.height = '80px';
+     texto.style.height = texto.scrollHeight + 'px';
+     boxRight.style.height = '200px';
+     boxRight.style.height = boxRight.scrollHeight + 'px';
+   }
+ });
+
+
+ // Agregando la hora
+ var timeDate = function () {
+   var f = new Date();
+   var time = f.getHours() + ":" + f.getMinutes();
+   var timeAbsolute = '';
+   if (f.getHours() <= 12) {
+     timeAbsolute = time + ' AM';
+   }else {
+     timeAbsolute = time + ' PM';
+   }
+   return timeAbsolute;
+ }
