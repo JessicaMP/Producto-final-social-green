bab8d3c9f2b4aa8ea7d2256c298119578f96fd22
JessicaMP <jessica95.mp@gmail.com>
2018-01-10 11:37:57 -0500

    Agregando iconos

--- js/perfil.js
+++ js/perfil.js
@@ -3,47 +3,54 @@ $(document).ready(function(){
    $(".button-collapse").sideNav();
    $('.modal').modal();
  });
-
+// Post de texto
  $('#btn-share-post').click(function() {
    var firstBox= $('<div></div>');
    var content = $('<p></p>');
+   var icon = $('<i></i>');
+   var icon2 = $('<i></i>');
+   var icon3 = $('<i></i>');
+
    content.text($('#texto').val());
+   icon.text('favorite');
+   icon2.text('thumb_up');
+   icon3.text('thumb_down');
+
    var time = $('<p></p>');
    time.text(timeDate());
 
    firstBox.addClass('styleBox');
    content.addClass('styleLetter');
    time.addClass('styleLetter');
+   icon.addClass('material-icons');
+   icon2.addClass('material-icons');
+   icon3.addClass('material-icons');
 
-   if ($('#texto').val('') == true) {
-     console.log(  $('this'));
-     $('this').find('#button').disabled();
-
-   }else{
+   if ($('#texto').val('')) {
      firstBox.append(content);
      firstBox.append(time);
+     firstBox.append(icon);
+     firstBox.append(icon2);
+     firstBox.append(icon3);
      $('#show-post').append(firstBox);
-
    }
+
    $('#texto').val(' ');
-   texto.focus();
+   $('#texto').focus();
  });
-
- //Cambio de color del boton al ingresar texto
- texto.addEventListener('keyup', function() {
-   var contenido = texto.value;
-   var length = texto.value.lenght;
-   if (contenido == '') {
-     button.className = 'Original';
-   }else {
-     button.className = 'buttonColor';
-     texto.style.height = '80px';
-     texto.style.height = texto.scrollHeight + 'px';
-     boxRight.style.height = '200px';
-     boxRight.style.height = boxRight.scrollHeight + 'px';
-   }
- });
-
+$('#texto').keyup(function() {
+  var contenido = $('#texto').val();
+  var length = $('#texto').val().lenght;
+  if (contenido == '') {
+    $('#btn-share-post').addClass('transparent');
+  }else {
+    $('#btn-share-post').addClass('red')
+    $('#texto').css('height', '80px');
+    $('#texto').css('height', $('#texto').scrollHeight + 'px');
+    $('.box-right').css('height', '200px');
+    $('.box-right').css('height', $('.box-right').scrollHeight + 'px');
+  }
+});
 
  // Agregando la hora
  var timeDate = function () {
